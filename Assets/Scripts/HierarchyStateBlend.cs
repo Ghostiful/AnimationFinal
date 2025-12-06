8b04465d5dd56e16daa393a9973972ac36de77e3
Isabel Rowland <isabel.rowland@mymail.champlain.edu>
2025-12-06 16:21:02 -0500

    Added node configuration and execute function to blend tree

--- Assets/Scripts/HierarchyStateBlend.cs
+++ Assets/Scripts/HierarchyStateBlend.cs
@@ -1,19 +1,128 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
-public class HierarchyStateBlend : MonoBehaviour
+public static class HierarchyStateBlend
 {
-    // Start is called before the first frame update
-    void Start()
+    public static int a3spatialPoseBlendTreeConfigureNode(a3_SpatialPoseBlendTree blendTree, int nodeIndex, a3_SpatialPose outPose, a3_SpatialPose pose1, a3_SpatialPose pose2, a3_BlendOpSet blendOp)
     {
-        
+        blendTree.nodes[nodeIndex].pose_out = outPose;
+        blendTree.nodes[nodeIndex].pose_ctrl[0] = pose1;
+        blendTree.nodes[nodeIndex].pose_ctrl[1] = pose2;
+        blendTree.nodes[nodeIndex].vCount = 2;
+        blendTree.nodes[nodeIndex].blendOpSet = blendOp;
+
+        return 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static int a3spatialPoseBlendTreeExecute(a3_SpatialPoseBlendTree blendTree)
     {
-        
+        for (uint i = 0; i < blendTree.blendTreeDescriptor.numNodes; i++)
+        {
+            a3_BlendOp op = new a3_BlendOp();
+            float u = 0.5f;
+
+            // Set input
+            blendTree.nodes[i].SetInput(0, new float[] { u });
+            blendTree.nodes[i].uCount = 1;
+
+            /// ROTATION BLEND
+            op.op = blendTree.nodes[i].blendOpSet.op_rotate;
+            op.v_out = GetVector4AsArray(ref blendTree.nodes[i].pose_out.rotate);
+
+            // Set controls
+            for (uint j = 0; j < blendTree.nodes[i].vCount; j++)
+            {
+                op.v_ctrl[j] = GetVector4AsArray(ref blendTree.nodes[i].pose_ctrl[j].rotate);
+            }
+            op.vCount = blendTree.nodes[i].vCount;
+
+            // Set inputs
+            for (uint j = 0; j < blendTree.nodes[i].uCount; j++)
+            {
+                op.u[j] = blendTree.nodes[i].GetInput((int)j);
+            }
+            op.uCount = blendTree.nodes[i].uCount;
+
+            // Execute blend operation
+            op.exec = blendTree.nodes[i].blendOpSet.exec;
+            blendTree.nodes[i].blendOpSet.exec(op);
+
+            // Write back result
+            SetVector4FromArray(ref blendTree.nodes[i].pose_out.rotate, op.v_out);
+
+            /// TRANSLATION BLEND
+            op.op = blendTree.nodes[i].blendOpSet.op_translate;
+            op.v_out = GetVector4AsArray(ref blendTree.nodes[i].pose_out.translate);
+
+            // Set controls
+            for (int j = 0; j < blendTree.nodes[i].vCount; j++)
+            {
+                op.v_ctrl[j] = GetVector4AsArray(ref blendTree.nodes[i].pose_ctrl[j].translate);
+            }
+            op.vCount = blendTree.nodes[i].vCount;
+
+            // Set inputs
+            for (int j = 0; j < blendTree.nodes[i].uCount; j++)
+            {
+                op.u[j] = blendTree.nodes[i].GetInput(j);
+            }
+            op.uCount = blendTree.nodes[i].uCount;
+
+            // Execute blend operation
+            op.exec = blendTree.nodes[i].blendOpSet.exec;
+            blendTree.nodes[i].blendOpSet.exec(op);
+
+            // Write back result
+            SetVector4FromArray(ref blendTree.nodes[i].pose_out.translate, op.v_out);
+
+            /// SCALE BLEND
+            op.op = blendTree.nodes[i].blendOpSet.op_scale;
+            op.v_out = GetVector4AsArray(ref blendTree.nodes[i].pose_out.scale);
+
+            // Set controls
+            for (int j = 0; j < blendTree.nodes[i].vCount; j++)
+            {
+                op.v_ctrl[j] = GetVector4AsArray(ref blendTree.nodes[i].pose_ctrl[j].scale);
+            }
+            op.vCount = blendTree.nodes[i].vCount;
+
+            // Set inputs
+            for (int j = 0; j < blendTree.nodes[i].uCount; j++)
+            {
+                op.u[j] = blendTree.nodes[i].GetInput(j);
+            }
+            op.uCount = blendTree.nodes[i].uCount;
+
+            // Execute blend operation
+            op.exec = blendTree.nodes[i].blendOpSet.exec;
+            blendTree.nodes[i].blendOpSet.exec(op);
+
+            // Write back result
+            SetVector4FromArray(ref blendTree.nodes[i].pose_out.scale, op.v_out);
+        }
+
+        return 1;
+
+    }
+
+    // Helper: Convert Vector4 to float array
+    private static float[] GetVector4AsArray(ref Vector4 vec)
+    {
+        return new float[] { vec.x, vec.y, vec.z, vec.w };
+    }
+
+    // Helper: Set Vector4 from float array
+    private static void SetVector4FromArray(ref Vector4 vec, float[] array)
+    {
+        if (array != null && array.Length >= 4)
+        {
+            vec.x = array[0];
+            vec.y = array[1];
+            vec.z = array[2];
+            vec.w = array[3];
+        }
     }
 }
 
@@ -60,13 +169,13 @@ public class a3_BlendOp
 
 public struct a3_BlendOpSet
 {
-    a3blendOpExecute exec;
-    a3realOp op_transformMat;
-    a3realOp op_transformDQ;
-    a3realOp op_rotate;
-    a3realOp op_scale;
-    a3realOp op_translate;
-    a3realOp op_user;
+    public a3blendOpExecute exec;
+    public a3realOp op_transformMat;
+    public a3realOp op_transformDQ;
+    public a3realOp op_rotate;
+    public a3realOp op_scale;
+    public a3realOp op_translate;
+    public a3realOp op_user;
 }
 
 public class a3_SpatialPoseBlendNode
@@ -105,6 +214,13 @@ public class a3_SpatialPoseBlendNode
             if (index >= uCount)
                 uCount = (ushort)(index + 1);
         }
+    }
+
+    public float[] GetInput(int index)
+    {
+        if (index >= 0 && index < uCount)
+            return u[index];
+        return null;
     }
 
     public a3_SpatialPose GetControlPose(int index)
